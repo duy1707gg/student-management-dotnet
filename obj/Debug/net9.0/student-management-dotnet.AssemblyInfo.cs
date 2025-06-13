389c7a6879cc27ec04a8e5bb426878e9d9ede3af
duy1707gg <duyhl2005@gmail.com>
2025-06-13 12:12:51 +0700

    v7

--- obj/Debug/net9.0/student-management-dotnet.AssemblyInfo.cs
+++ obj/Debug/net9.0/student-management-dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("student-management-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e9e182dff3646451f9a456831e2e896186f6953")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15281cb9d9f788bd18f43c3e090d80f152c49255")]
 [assembly: System.Reflection.AssemblyProductAttribute("student-management-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("student-management-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
