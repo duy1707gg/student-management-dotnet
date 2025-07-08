269bacb5c2d2d16dcb9beab720d245eb214c16f2
duy1707gg <duyhl2005@gmail.com>
2025-07-08 14:58:50 +0700

    🔧 Remove artifacts folder to avoid infinite publish loops

--- obj/Debug/net9.0/student-management-dotnet.AssemblyInfo.cs
+++ obj/Debug/net9.0/student-management-dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("student-management-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dfbeadd2de476049423f9edb5bbe75066f10c6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5413ae692993881306b73ce071f6fc5f3859c0da")]
 [assembly: System.Reflection.AssemblyProductAttribute("student-management-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("student-management-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
